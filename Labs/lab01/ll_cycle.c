1a41e64e5265ee90104463dd5a32b544682e7b4e
GaryAgasa <garyagasa547@gmail.com>
2025-06-29 22:18:37 +0800

    Initial commit: CS61C 2020 Fall framework

---
+++ Labs/lab01/ll_cycle.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "ll_cycle.h"
+
+int ll_has_cycle(node *head) {
+    /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+    while(1){
+       hare = hare -> next;
+       if(hare == NULL){
+        return 0;
+       }
+       hare = hare -> next;
+       if(hare == NULL){
+        return 0;
+       }
+       tortoise = tortoise -> next;
+       if(tortoise == hare){
+        return 1;
+       }
+    }
+    return -1;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CS61C_2020fall_GA
+This repository contains all the labs and projects' code solutions for the CS61C 2020 Fall, which are completed by Gary Agasa.
