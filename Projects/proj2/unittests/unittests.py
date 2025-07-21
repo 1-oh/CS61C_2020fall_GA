05399b34e44bc249d74f5536ad037abca1c39453
GaryAgasa <garyagasa547@gmail.com>
2025-07-21 14:25:08 +0800

    Update the test

--- Projects/proj2/unittests/unittests.py
+++ Projects/proj2/unittests/unittests.py
@@ -50,6 +50,21 @@ class TestRelu(TestCase):
         # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
         t.execute()
 
+    def test_complicated(self):
+        t = AssemblyTest(self, "relu.s")
+        # create an array in the data section
+        array0 = t.array([1, -1, 4, -5, -1, 4, 0, 2, -9, 5, -2, 20])
+        # load address of `array0` into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the relu function
+        t.call("relu")
+        # check that the array0 was changed appropriately
+        t.check_array(array0, [1, 0, 4, 0, 0, 4, 0, 2, 0, 5, 0, 20])
+        # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
+        t.execute()
+
     @classmethod
     def tearDownClass(cls):
         print_coverage("relu.s", verbose=False)
@@ -59,16 +74,20 @@ class TestArgmax(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        raise NotImplementedError("TODO")
         # TODO
+        array0 = t.array([1, -1, 4, -5, -1, 4, 10, 2, -9, 5, -2])
         # load address of the array into register a0
         # TODO
+        t.input_array("a0", array0)
         # set a1 to the length of the array
         # TODO
+        t.input_scalar("a1", len(array0))
         # call the `argmax` function
         # TODO
+        t.call("argmax")
         # check that the register a0 contains the correct output
         # TODO
+        t.check_scalar("a0", 6)
         # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
         t.execute()
 
